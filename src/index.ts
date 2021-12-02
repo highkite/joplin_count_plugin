61bacf717a62aadcd5cefd1a91d79fea8d69f974
Thomas Osterland <highway.ita07@web.de>
2021-12-02 22:57:32 +0100

    write count to note

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,34 @@ function hasNodeCountHeader(noteBody : string) : boolean {
 }
 
 function extractCountData(noteBody : string) : CountItem[] {
-    return [{title : "", count : 0}]
+    const lines = noteBody.split('\n');
+    let values : CountItem[] = []
+
+    for (const line of lines) {
+        const match = line.match(/^([^:]*)[:\s]+(\d*).*/);
+        if(!match) continue
+        values.push({title: match[1], count: +match[2]})
+    }
+
+    return values
 }
 
+function updateNote(item : CountItem) {
+    joplin.workspace.selectedNote().then((note) => {
+        const lines = note.body.split('\n');
+
+        let new_doc = []
+
+        for (const line of lines) {
+            if (line.startsWith(item.title)) {
+                new_doc.push(`${item.title}: ${item.count}`)
+            } else {
+                new_doc.push(line)
+            }
+        }
+        joplin.data.put(['notes', note.id], null, { body: new_doc.join("\n") }).then(console.log)
+    })
+}
 
 joplin.plugins.register({
     onStart: async function() {
@@ -24,6 +49,14 @@ joplin.plugins.register({
         await joplin.views.panels.setHtml(panel, '<canvas id="countLayer"></canvas>')
         await joplin.views.panels.addScript(panel, './webview.js')
         await joplin.views.panels.hide(panel)
+
+        await joplin.views.panels.onMessage(panel, (message) => {
+            if (message.name === 'clickedTable') {
+                updateNote(message.data)
+            } else if (message.name === 'clickedTableSub') {
+                updateNote(message.data)
+            }
+        });
 
         async function setupViewPane() {
             console.info("Setup view pane")
@@ -33,6 +66,7 @@ joplin.plugins.register({
                 await joplin.views.panels.show(panel)
 
                 let data = extractCountData(note.body)
+                await joplin.views.panels.setHtml(panel, '<p id="dataContent" hidden>' + JSON.stringify(data) + '</p><canvas id="countLayer"></canvas>')
             } else {
                 await joplin.views.panels.hide(panel)
             }
--- src/webview.js
+++ src/webview.js
@@ -1,7 +1,9 @@
-let data = [{title: "Item 1", count: 0}, {title: "Item 2", count: 2}, {title: "Item 3", count: 5}, {title: "Item 4", count: 7}]
+//let data = [{title: "Item 1", count: 0}, {title: "Item 2", count: 2}, {title: "Item 3", count: 5}, {title: "Item 4", count: 7}]
+let data = undefined
 const STROKE_HEIGHT = 50
 const STROKE_SPACING = 10
 const STROKE_SET_WIDTH = 50
+let canvas = undefined
 
 function drawFiveStroke(ctx, strokeCount, x, y) {
   ctx.beginPath()
@@ -22,24 +24,80 @@ function drawFiveStroke(ctx, strokeCount, x, y) {
 
 function renderStrokeInArea(ctx, strokeCount, x, y, width, height) {
   let x_offset = x + 5, y_offset = y + STROKE_HEIGHT + 5
+  let used_width = STROKE_SET_WIDTH;
   for (let i = 0; i < Math.floor(strokeCount / 5); ++i) {
     drawFiveStroke(ctx, 5, x_offset, y_offset)
+    used_width += STROKE_SET_WIDTH + 10
 
     x_offset += STROKE_SET_WIDTH + 10
 
-    if (i * STROKE_SET_WIDTH > width) {
-      x_offset = x
+    if (used_width > width) {
+      x_offset = x + 5
       y_offset += STROKE_HEIGHT + 5
+      used_width = STROKE_SET_WIDTH
     }
   }
 
   drawFiveStroke(ctx, strokeCount % 5, x_offset, y_offset)
 }
 
+function getData() {
+  let dataField = document.getElementById("dataContent")
+
+  if(dataField) {
+    data = JSON.parse(dataField.textContent)
+    renderPanel()
+  } else {
+    setTimeout(getData, 1000)
+  }
+}
+
+function increment(event) {
+  let x = event.clientX
+  let y = event.clientY
+
+  let selectedItem = getItemFromCoordinates(x, y)
+  selectedItem.count++
+  webviewApi.postMessage({
+    name: 'clickedTable',
+    data: selectedItem
+  });
+
+  canvas.removeEventListener('click', increment)
+  canvas.removeEventListener('contextmenu', decrement)
+  setTimeout(getData, 500)
+}
+
+function decrement(event) {
+  let x = event.clientX
+  let y = event.clientY
+
+  if(selectedItem.count > 0) {
+    selectedItem.count--
+  }
+  let selectedItem = getItemFromCoordinates(x, y)
+  webviewApi.postMessage({
+    name: 'clickedTableSub',
+    data: selectedItem
+  });
+
+  canvas.removeEventListener('click', increment)
+  canvas.removeEventListener('contextmenu', decrement)
+  setTimeout(getData, 500)
+}
+
 function renderPanel() {
   console.info("HEEEEEEEEEE>>>>>>>>>>>>>>>>>>>>>>>>>>")
-  let canvas = document.getElementById("countLayer")
+  if(!data) {
+    getData()
+    return
+  }
+
+  canvas = document.getElementById("countLayer")
   let height = Math.ceil(data.length / 2) * 300
+
+  canvas.addEventListener('click', increment)
+  canvas.addEventListener('contextmenu', decrement)
 
   canvas.width = document.width > 600 ? document.width : 600
   canvas.height = height
@@ -71,9 +129,17 @@ function renderPanel() {
     ctx.stroke()
 
     renderStrokeInArea(ctx, data[i].count, 0, y_offset_text, cell_width, 250)
-    renderStrokeInArea(ctx, data[i + 1].count, cell_width, y_offset_text, canvas.width, 250)
+    renderStrokeInArea(ctx, data[i + 1].count, cell_width, y_offset_text, cell_width, 250)
   }
 }
 
-//window.onload = renderPanel
+function getItemFromCoordinates(x, y) {
+  let line = Math.floor(y / 300)
+  let column = Math.floor(x / (canvas.width / 2))
+
+  return data[line * 2 + column]
+}
+
+
+
 renderPanel()
